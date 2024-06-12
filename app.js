de84ebe87ebe6c02484d825b74e5977d02eb45d5
Sadeepa Dilhara <sadeepadilharap@gmail.com>
2024-06-12 15:33:03 +0530

    Weather according to input done

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 const { log } = require("console");
-const express =  require("express");
+const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req,res){
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html")
 
-    const url ="https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=465bc8318d500a53b0b93e10d74ca723";
-    https.get(url,function(response){
+})
+
+app.post("/", function (req, res) {
+    console.log("Post received")
+    const query = req.body.cityName;;
+    const apiKey = "465bc8318d500a53b0b93e10d74ca723";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey;
+    https.get(url, function (response) {
         console.log(response.statusCode);
 
-        response.on("data", function(data){
-            const weatherData=JSON.parse(data);
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const icon = weatherData.weather[0].icon;
             const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
             const weatherDescription = weatherData.weather[0].description;
             console.log(weatherDescription);
-        
-            res.write("<p>Weathe is like " + weatherDescription +  "</p>");
-            res.write("<h1>The temp is london is " + temp + " degree celcius</h1>");
-            res.write("<image src=" +imageURL +">");
+
+            res.write("<p>Weathe is like " + weatherDescription + "</p>");
+            res.write("<h1>The temp is "+ query+" is " + temp + " degree celcius</h1>");
+            res.write("<image src=" + imageURL + ">");
             res.send();
         })
     })
@@ -33,6 +43,6 @@ app.get("/", function(req,res){
 
 
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server is running on port 3000.");
 })
